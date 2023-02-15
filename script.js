e670c3619b20228c1b06cd9ac5f77e5d367a4feb
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-02-15 09:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-//your code here
-const inputElement = document.getElementTarget("input")[0];
-const inputElement = inputElement.value;
-const arr= input.split(',');
+// //your code here
+// const inputElement = document.getElementTarget("input")[0];
+// const inputElement = inputElement.value;
+// const arr= input.split(',');
 
-console.log(arr);
+// console.log(arr);
+
+function minCostToFormRope(event) {
+	event.preventDefault();
+	var inputElement= document.querySelector('input').value;
+	var arr=inputElement.split(',');
+	arr.sort(function(a,b){return a-b});
+
+	var cost=0; 
+	while(arr.length>1){
+		var res = Number(arr[0])+Number(arr[1]);
+		arr.push(res);
+		cost+=res;
+		arr.shift();
+		arr.shift();
+
+		arr.sort(function(a,b){return a-b});
+		
+	}
+	document.getElementById("result").textContent=cost;
+}
